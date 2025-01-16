b6ac64cb6948cc6e524311c215bef3aeac12f323
Priyanshu Sharma <priyanshu.sh18@gmail.com>
2025-01-16 20:47:55 +0530

    update navbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -19,7 +19,7 @@ export const Appbar = ()=>{
                 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 
                 text-center me-2 mb-2">Publish</button>
             </Link>
-            <Avatar size="big" name="Priyanshu"/>
+            <Avatar size="big" name="User"/>
         </div>
     </div>
 }
